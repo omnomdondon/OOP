95491e34dfe6c87116ff8b9c05f34884c21479e3
Brandon Kenneth Cinco <112753462+omnomdondon@users.noreply.github.com>
2023-06-04 07:49:32 +0800

    Create Parents.java

---
+++ Quizzes/Quiz1/Parents.java
@@ -0,0 +1,17 @@
+package Quiz1;
+
+public class Parent {
+	
+	// Attributes that Child1 and Child2 inherits.
+	String attitude, hairColor, bloodType, property;
+	
+	/* info() method to display the parent's attributes when it is called 
+	 * Child1 and Child2 also inherits this method*/
+	void attributes() {
+		System.out.println("Attitude   : " + attitude);
+		System.out.println("Hair color : " + hairColor);
+		System.out.println("Blood type : " + bloodType);
+		System.out.println("Property   : " + property);
+	}
+	
+}
