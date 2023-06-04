fa019eaf7b21313ff368b99d69c19c5801bd49c5
Brandon Kenneth Cinco <112753462+omnomdondon@users.noreply.github.com>
2023-06-04 08:01:55 +0800

    Create Person.java

---
+++ Others/Demonstrations/Polymorphism/Person.java
@@ -0,0 +1,15 @@
+package Polymorphism;
+
+public class Person {
+	
+	String talent, voiceSound;
+	
+	void talent() {
+		System.out.println("I " + talent);
+	}
+	
+	void voiceSound() {
+		System.out.println("My voice is " + voiceSound);
+	}
+	
+}
