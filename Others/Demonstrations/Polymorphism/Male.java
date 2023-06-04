a7dd6798f232f528a6dcaca06cf9883e2e579eee
Brandon Kenneth Cinco <112753462+omnomdondon@users.noreply.github.com>
2023-06-04 08:29:15 +0800

    Update Male.java

--- Others/Demonstrations/Polymorphism/Male.java
+++ Others/Demonstrations/Polymorphism/Male.java
@@ -2,4 +2,11 @@ package Polymorphism;
 
 public class Male extends Person {
 	
+	void voiceSound() {
+		System.out.println("Husky");
+	}
+	
+	void talent() {
+		System.out.println("I can sing");
+	}
 }
